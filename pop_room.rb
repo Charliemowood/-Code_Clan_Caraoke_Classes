23941609c2e0debc919fb4b7c269b69feffdb410
Charlie Wood <charliemowood@gmail.com>
2017-04-21 16:42:33 +0100

    Add checked_in and checked_out method

--- pop_room.rb
+++ pop_room.rb
@@ -1,4 +1,32 @@
+require_relative('./songs.rb')
+require_relative('./guests.rb')
+
 class PopRoom
 
+  def setup
+    @guest1 = Guest.new("Charlie")
+    @song1 = Songs.new("In my Head")
+    @pop_room1 = PopRoom.new()
+  end
 
+  def initialize
+    @guests = []
+  end
+
+  def checked_in(guest)
+    @guests << guest
+    return @guests.count
+  end
+
+  def checked_out(guest)
+    @guests.delete(guest)
+    return @guests.count
+  end
+#
+#   def run_karaoke
+#     @pop_room1.checked_in(@guest1)
+# #   @song1.plays
+# #   @pop_room1.checked_out(@guest1)
+# #   return "The party is on!"
+#   end
 end
---
+++ specs/pop_room_spec.rb
@@ -0,0 +1,34 @@
+require('minitest/autorun')
+require('minitest/emoji')
+require_relative('../pop_room')
+require_relative('../songs')
+require_relative('../guests')
+
+class TestPopRoom < MiniTest::Test
+
+ def setup
+   @guest1 = Guest.new("Charlie")
+   @song1 = Songs.new("In my Head")
+   @pop_room1 = PopRoom.new()
+ end
+
+ def test_class_PopRoom_exists
+   refute_nil(@pop_room1)
+ end
+
+ def test_guest_can_check_in_to_PopRoom
+   assert_equal(1, @pop_room1.checked_in(@guest1))
+ end
+
+ def test_guest_can_check_out_of_PopRoom
+   @pop_room1.checked_in(@guest1)
+   assert_equal(0, @pop_room1.checked_out(@guest1))
+ end
+ #
+ def test_run_karaoke
+     @pop_room1.checked_in(@guest1)
+     @song1.plays
+     @pop_room1.checked_out(@guest1)
+     puts "The party is on!"
+ end
+end
