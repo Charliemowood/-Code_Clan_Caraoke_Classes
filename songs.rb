9d396aae6b19cfb6f81b9c72207c31fedc9808a6
Charlie Wood <charliemowood@gmail.com>
2017-04-21 12:06:39 +0100

    Can add song name to songs class

--- songs.rb
+++ songs.rb
@@ -1,4 +1,7 @@
 class Songs
+  attr_reader :name
 
-
+  def initialize(song_name)
+    @name = song_name
+  end
 end
--- specs/songs_spec.rb
+++ specs/songs_spec.rb
@@ -5,10 +5,14 @@ require_relative('../songs')
 class TestSongs < MiniTest::Test
 
   def setup
-    @song1 = Songs.new()
+    @song1 = Songs.new("In my Head")
   end
 
   def test_songs_class_exists
     refute_nil(@song1)
+  end
+
+  def test_song_has_name
+    assert_equal("In my Head", @song1.name)
   end
 end
